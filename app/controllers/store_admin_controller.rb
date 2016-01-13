1dee1474a510f03404c63355f0537fa31eb05e24
hendrikswan <hendrik.swanepoel@gmail.com>
2016-01-13 23:36:30 +0200

    StoreAdminController and admin layout

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,5 @@
+class StoreAdminController < ApplicationController
+  layout 'admin'
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get 'admin/home', to: 'store_admin#home'
 end
