c744f389ff21ca1763c4c705b1a95991004a8439
hendrikswan <hendrik.swanepoel@gmail.com>
2016-01-14 08:50:30 +0200

    specs for specifying a specific layout from an action render

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe StaticController, type: :controller do
   render_views
 
   describe "GET home" do
-    it "renders the home template" do
+    it "renders the home template with the default layout" do
       get :home
       expect(response).to render_template("home")
       expect(response).to render_template("layouts/application")
-      expect(response.body).to match /Welcome to FlatIron Widgets/im
-      expect(response.body).to match /Flatiron Widgets Store/im
+
+      assert_select "h1", "Flatiron Widgets Store"
+      assert_select "h2", "Welcome to Flatiron Widgets"
     end
   end
 end
--- spec/controllers/store_admin_controller_spec.rb
+++ spec/controllers/store_admin_controller_spec.rb
@@ -4,12 +4,25 @@ RSpec.describe StoreAdminController, type: :controller do
   render_views
 
   describe "GET home" do
-    it "renders the home template" do
+    it "renders the home template with admin layout" do
       get :home
       expect(response).to render_template("home")
       expect(response).to render_template("layouts/admin")
-      expect(response.body).to match /Welcome FlatIron Admin/im
-      expect(response.body).to match /Flatiron Widgets: Admin/im
+
+      assert_select "h1", "Flatiron Widgets: Admin"
+      assert_select "h2", "Welcome Flatiron Admin"
+    end
+  end
+
+  describe "GET orders" do
+    it "renders the orders template with order_administration layout" do
+      get :orders
+      expect(response).to render_template("orders")
+      expect(response).to render_template("layouts/order_administration")
+
+      assert_select "h1", "Flatiron Widgets: Open Orders"
+      assert_select "h2"
+      assert_select "ol"
     end
   end
 end
