f8c70a645790dbf614718837ff5fcab57b15825e
hendrikswan <hendrik.swanepoel@gmail.com>
2016-01-14 08:51:40 +0200

    controller and layout code for rendering a specific layout from an action

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -2,4 +2,8 @@ class StoreAdminController < ApplicationController
   layout 'admin'
   def home
   end
+
+  def orders
+    render :layout => "order_administration"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
+  get 'admin/orders', to: 'store_admin#orders'
 end
