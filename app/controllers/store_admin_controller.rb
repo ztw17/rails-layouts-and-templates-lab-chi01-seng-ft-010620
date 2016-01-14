7d30b01f7a187be9b3addb43187e56666576c67a
hendrikswan <hendrik.swanepoel@gmail.com>
2016-01-14 09:33:26 +0200

    files for rendering invoice action without a layout

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -6,4 +6,8 @@ class StoreAdminController < ApplicationController
   def orders
     render :layout => "order_administration"
   end
+
+  def invoice
+    render :layout => false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
+  get 'admin/invoice', to: 'store_admin#invoice'
 end
