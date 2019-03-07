ca55bddc9bf23a3f0ea6017a8ace9d3545d11f04
maxwellbenton <maxwellbenton@gmail.com>
2019-03-07 11:38:01 -0500

    merge with wip-rails-update-solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
